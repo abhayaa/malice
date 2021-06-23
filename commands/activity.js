dcd5b23e57f7e642a496d94c8d0963d7cb43516e
Abhaya Basnet <basnetabhaya@gmail.com>
2021-06-23 18:38:29 -0500

    model file name

--- commands/activity.js
+++ commands/activity.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const mongoose = require('mongoose');
-const profileModel = require('../models/activity');
+const profileModel = require('../models/activitymodel');
 
 
 module.exports = {
@@ -9,47 +9,57 @@ module.exports = {
     permissions: [],
     description: "get member activity by messages",
     async execute(message, client, args, Discord, profileData){
+
+        async function getUser(usr){
+            return await message.guild.members.fetch(usr);
+        }
+
+        console.log("executing");
         if(!args[0]){
-            const response = await profileModel.find({},{
+            console.log("running activity");
+            const response =  await profileModel.find({},{
                 "userID": 1,
                 "messagesSent": 1
                 }).limit(10).sort({
                     "messagesSent":1
                 });
-        
+
                 num = 1;
                 let embed = new Discord.MessageEmbed()
                     .setAuthor("Malice Activity Monitor")
                     .setColor("#427318")
-                    .setThumbnail(client.user.avatalURL)
+                    .setThumbnail(client.user.avatarURL)
                     .setTimestamp(Date.now());
+
                 response.forEach(entry => {
-                    embed.addField(num + '.', `User ` + `${message.guild.members.cache.get(entry.userID)}` +
+                    user = getUser(entry.userID);
+                    embed.addField(num + '.', `User ` + `${user}` +
                         ` with ` + entry.messagesSent + ` messages.`);
                     num+=1;
                 });
-        
                 message.channel.send(embed);
+
         }else if(args[0] === "roles"){
+            console.log("running activity by roles");
             const response = await profileModel.find({},{
                 "userID": 1,
-                "messagesSent": 1
+                "roles": 1
                 }).limit(10).sort({
-                    "roles":1
+                    "roles": 1
                 });
-        
+
                 num = 1;
                 let embed = new Discord.MessageEmbed()
                     .setAuthor("Malice Activity Monitor")
                     .setColor("#427318")
-                    .setThumbnail(client.user.avatalURL)
+                    .setThumbnail(client.user.avatarURL)
                     .setTimestamp(Date.now());
+
                 response.forEach(entry => {
-                    embed.addField(num + '.', `User ` + `${message.guild.members.cache.get(entry.userID)}` +
-                        ` with ` + `${message.guild.members.cache.get(entry.userID).roles.cache.size}` + ` roles.`);
+                    embed.addField(num + '.', `User ` + `${message.guild.members.fetch(entry.userID)}` +
+                        ` with ` + entry.roles + ` roles.`);
                     num+=1;
                 });
-        
                 message.channel.send(embed);
         }
     },
--- commands/memberUpdate.js
+++ commands/memberUpdate.js
@@ -1,34 +1,33 @@
-const profileModel = require('../models/activity')
+const profileModel = require('../models/activitymodel')
 require('dotenv').config()
 
 module.exports = {
     name: "memberupdate",
     alias: [],
     permissions: [],
-    description: "get member activity by messages",
+    description: "Get member activity by messages",
     async execute(message, client, args, Discord, profileData){
         num = 0;
-        message.guild.members.cache.each(member=>{
-            if(!profileData && !message.author.bot){
+        message.guild.members.fetch()
+        .then(member =>{
+            if(!member.user.bot){
+                console.log("adding new");
                 let profile = profileModel.create({
-                    userID: member.id,
-                    username: member.username,
+                    userID: member.user.id,
+                    username: member.user.username,
                     nessagesSent:1,
-                    roles: member.roles.cache.size
-                    
+                    roles: member.user.roles.cache.size
                 })
-            }else if(profileData && !message.author.bot){
-                const respose = profileModel.findOneAndUpdate({
-                    userID: member.id,
+            }else if(data && !member.user.bot){
+                console.log("adding new");
+                const profile = profileModel.findOneAndUpdate({
+                    userID: member.user.id,
                 },{
-                    roles: member.roles.cache.size
+                    roles: member.user.roles.cache.size
                 })
             }
-
             num+=1;
         });
-        
-        message.channel.send("All members updated with number of roles they have: ".concat(num));
-        
+        message.channel.send("Members role updated ".concat(num));
     },
 };
